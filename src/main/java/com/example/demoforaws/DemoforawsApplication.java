274f89c628c469da2902b573619d5f2e1df2c3b8
nagendra080389 <nagendra080389@gmail.com>
2023-07-06 19:51:21 +0530

    Initial commit

---
+++ src/main/java/com/example/demoforaws/DemoforawsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoforaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoforawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoforawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoforaws/DemoforawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoforaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoforawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
